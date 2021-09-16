e37de688008a95491e5a9e43b0dd1a233ff50bff
emikhaylov <emikhaylov@diasoft.ru>
2021-09-16 17:53:38 +0500

    initial

---
+++ library/service/src/test/java/net/mikhailov/books/library/LibraryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.mikhailov.books.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
