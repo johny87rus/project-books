a4f7715677d0537ba99a2489dbeb6c0175c6574d
emikhaylov <emikhaylov@diasoft.ru>
2021-09-17 13:23:36 +0500

    Добавил Entity

---
+++ library/src/main/java/net/mikhailov/books/library/model/Book.java
@@ -0,0 +1,41 @@
+package net.mikhailov.books.library.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "book")
+@SequenceGenerator(name = Book.SEQUENCE_NAME, allocationSize = 1, initialValue = 1000)
+public class Book {
+    public static final String SEQUENCE_NAME = "book_seq";
+
+    @Id
+    @GeneratedValue(generator = SEQUENCE_NAME, strategy = GenerationType.SEQUENCE)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @ManyToMany
+    @JoinTable(name = "book_author", joinColumns = @JoinColumn(name = "bookid"), inverseJoinColumns = @JoinColumn(name = "authorid"))
+    List<Author> authors;
+
+    @OneToMany(mappedBy = "book")
+    List<ISBN> isbnList;
+}
