4b19ef7eec365eb78e736994e055fe0f35dc25e9
emikhaylov <emikhaylov@diasoft.ru>
2021-09-19 15:01:01 +0500

    Добавил тесты на BookController POST

--- library/src/main/java/net/mikhailov/books/library/dto/BookDTO.java
+++ library/src/main/java/net/mikhailov/books/library/dto/BookDTO.java
@@ -1,5 +1,7 @@
 package net.mikhailov.books.library.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,24 +12,32 @@ import java.util.List;
 
 @Getter
 @Setter
+@JsonPropertyOrder(alphabetic = true)
 public class BookDTO {
+    @JsonProperty("id")
     private Integer id;
 
     @NotBlank
+    @JsonProperty("bookname")
     private String title;
 
+    @JsonProperty("isbnlist")
+    @NotEmpty
     private List<Long> isbnList;
 
     @Valid
     @NotEmpty
+    @JsonProperty("authorlist")
     private List<AuthorDTO> authorList;
 
     @Getter
     @Setter
     public static class AuthorDTO {
         @NotBlank
+        @JsonProperty("name")
         private String authorName;
         @NotBlank
+        @JsonProperty("lastname")
         private String authorLastName;
     }
 
---
+++ library/src/test/java/net/mikhailov/books/library/controller/BookControllerImplTest.java
@@ -0,0 +1,132 @@
+package net.mikhailov.books.library.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.mikhailov.books.library.dto.BookDTO;
+import net.mikhailov.books.library.service.BookService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @author Mikhailov Evgenii
+ */
+@WebMvcTest
+@AutoConfigureMockMvc
+class BookControllerImplTest {
+    @MockBean
+    BookService bookService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void whenPostEmptyJSON() throws Exception {
+        String book = "{}";
+        mockMvc.perform(MockMvcRequestBuilders.post("/books")
+                        .content(book)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    void whenPostWithoutTitle() throws Exception {
+        String jsonTestString = "{\"authorlist\":[{\"name\":\"TestName\",\"lastname\":\"TestLastName\"}],\"bookname\":\"\",\"id\":null,\"isbnlist\":[1234567890123]}";
+        mockMvc.perform(MockMvcRequestBuilders.post("/books")
+                        .content(jsonTestString)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andExpect(content().json("{\"title\":\"must not be blank\"}"));
+    }
+
+    @Test
+    void whenPostWithoutISBN() throws Exception {
+        String jsonTestString = "{\"authorlist\":[{\"name\":\"TestName\",\"lastname\":\"TestLastName\"}],\"bookname\":\"bookTestName\",\"id\":null,\"isbnlist\":[]}";
+        mockMvc.perform(MockMvcRequestBuilders.post("/books")
+                        .content(jsonTestString)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andExpect(content().json("{\"isbnList\":\"must not be empty\"}"));
+    }
+
+    @Test
+    void whenPostWithoutAuthors() throws Exception {
+        String jsonTestString = "{\"authorlist\":[],\"bookname\":\"bookTestName\",\"id\":null,\"isbnlist\":[1234567890123]}";
+        mockMvc.perform(MockMvcRequestBuilders.post("/books")
+                        .content(jsonTestString)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andExpect(content().json("{\"authorList\":\"must not be empty\"}"));
+    }
+
+    @Test
+    void whenPostWithoutAuthorName() throws Exception {
+        String jsonTestString = "{\"authorlist\":[{\"name\":\"\",\"lastname\":\"TestLastName\"}],\"bookname\":\"bookTestName\",\"id\":null,\"isbnlist\":[1234567890123]}";
+        mockMvc.perform(MockMvcRequestBuilders.post("/books")
+                        .content(jsonTestString)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andExpect(content().json("{\"authorList[0].authorName\":\"must not be blank\"}"));
+    }
+
+    @Test
+    void whenPostWithoutAuthorLastName() throws Exception {
+        String jsonTestString = "{\"authorlist\":[{\"name\":\"Name\",\"lastname\":\"\"}],\"bookname\":\"bookTestName\",\"id\":null,\"isbnlist\":[1234567890123]}";
+        mockMvc.perform(MockMvcRequestBuilders.post("/books")
+                        .content(jsonTestString)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andExpect(content().json("{\"authorList[0].authorLastName\":\"must not be blank\"}"));
+    }
+
+    @Test
+    void whenPostValidBookDTO() throws Exception {
+        BookDTO bookDTO = new BookDTO();
+        bookDTO.setTitle("TestBook");
+        List<Long> isbnList = new ArrayList<>(1);
+        isbnList.add(123_45678_90123L);
+        bookDTO.setIsbnList(isbnList);
+
+        List<BookDTO.AuthorDTO> authorDTOList = new ArrayList<>(1);
+        BookDTO.AuthorDTO authorDTO = new BookDTO.AuthorDTO();
+        authorDTO.setAuthorName("TestName");
+        authorDTO.setAuthorLastName("TestLastName");
+        authorDTOList.add(authorDTO);
+        bookDTO.setAuthorList(authorDTOList);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/books")
+                        .content(objectMapper.writeValueAsString(bookDTO))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void whenPostValidJSON() throws Exception {
+        String jsonTestString = "{\"authorlist\":[{\"name\":\"TestName\",\"lastname\":\"TestLastName\"}],\"bookname\":\"TestBook\",\"id\":null,\"isbnlist\":[1234567890123]}";
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/books")
+                        .content(jsonTestString)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+}
