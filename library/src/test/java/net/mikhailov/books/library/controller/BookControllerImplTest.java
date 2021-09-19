93e24555c453bef486c061561919a3b5381041e4
emikhaylov <emikhaylov@diasoft.ru>
2021-09-19 15:19:42 +0500

    Удалил аннотацию AutoConfigureMockMvc

--- library/src/test/java/net/mikhailov/books/library/controller/BookControllerImplTest.java
+++ library/src/test/java/net/mikhailov/books/library/controller/BookControllerImplTest.java
@@ -6,7 +6,6 @@ import net.mikhailov.books.library.service.BookService;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -24,7 +23,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * @author Mikhailov Evgenii
  */
 @WebMvcTest
-@AutoConfigureMockMvc
 class BookControllerImplTest {
     @MockBean
     BookService bookService;
