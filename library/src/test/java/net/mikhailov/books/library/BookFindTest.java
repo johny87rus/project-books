d74697099dd0cf2df935d2f28fb6c824e42aa2b3
emikhaylov <emikhaylov@diasoft.ru>
2021-10-19 17:37:57 +0500

    Jsoup тесты

---
+++ library/src/test/java/net/mikhailov/books/library/BookFindTest.java
@@ -0,0 +1,30 @@
+package net.mikhailov.books.library;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import java.io.IOException;
+import java.util.Objects;
+
+/**
+ * @author Evgenii Mikhailov
+ */
+class BookFindTest {
+    private static final String URL = "https://www.bookfinder.com/search/?author=&title=&lang=en&new_used=*&destination=ru&currency=USD&mode=basic&st=sr&ac=qr&isbn=";
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/findBookTest.csv", delimiter = ':', lineSeparator = "\n")
+    void test(String input, String expected, String expectedDescription) throws IOException {
+        Document document = Jsoup
+                .connect(URL+input)
+                .method(Connection.Method.GET).get();
+        Assertions.assertEquals(expected, document.select("#describe-isbn-title").text(), () -> "Название книги не соответствует ISBN");
+        if (Objects.nonNull(expectedDescription)) {
+            Assertions.assertEquals(expectedDescription, document.select("#bookSummary").text(), () -> "Description не соответствует ISBN");
+        }
+    }
+}
