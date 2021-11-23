956d701a0bb600eaadd3a0fed08ffb2cc986fd21
evgenii <evgenii@mikhailov.net>
2021-11-23 09:33:54 +0500

    Put

--- library/src/main/java/net/mikhailov/books/library/service/AuthorService.java
+++ library/src/main/java/net/mikhailov/books/library/service/AuthorService.java
@@ -1,7 +1,6 @@
 package net.mikhailov.books.library.service;
 
 import net.mikhailov.books.model.AuthorDTO;
-import net.mikhailov.books.model.IdDTO;
 
 import java.util.List;
 
@@ -9,6 +8,7 @@ import java.util.List;
  * @author Mikhailov Evgenii
  */
 public interface AuthorService {
-    IdDTO postAuthor(AuthorDTO authorDTO);
+    AuthorDTO postAuthor(AuthorDTO authorDTO);
     List<AuthorDTO> getAllAuthors();
+    AuthorDTO putAuthor(Long id, AuthorDTO authorDTO);
 }
