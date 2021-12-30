c7678c26ec5a531b328838986bd35d34af2bf86f
evgenii <evgenii@mikhailov.net>
2021-12-30 10:46:05 +0500

    удаление по isbn

--- library/src/main/java/net/mikhailov/books/library/domain/book/BookRepository.java
+++ library/src/main/java/net/mikhailov/books/library/domain/book/BookRepository.java
@@ -2,6 +2,8 @@ package net.mikhailov.books.library.domain.book;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -10,4 +12,8 @@ public interface BookRepository extends JpaRepository<Book, Long> {
 
     @EntityGraph(attributePaths = {"authors"})
     List<Book> findAllBy();
+
+    @Query("delete from Book b where b.id = ?1 or b.isbn = ?1")
+    @Modifying
+    void deleteByIdOrIsbn(Long value);
 }
--- library/src/main/java/net/mikhailov/books/library/domain/book/BookServiceImpl.java
+++ library/src/main/java/net/mikhailov/books/library/domain/book/BookServiceImpl.java
@@ -78,7 +78,8 @@ public class BookServiceImpl implements BookService {
      * {@inheritDoc}
      */
     @Override
+    @Transactional
     public void deleteBook(Long bookId) {
-        repository.deleteById(bookId);
+        repository.deleteByIdOrIsbn(bookId);
     }
 }
