892541f2613e2c6036b11b9252bc4acbe5921486
evgenii <evgenii@mikhailov.net>
2022-01-11 19:06:36 +0500

    Разные возможности реализации конфига

---
+++ library/src/main/java/net/mikhailov/books/library/config/SecurityConfig.java
@@ -0,0 +1,65 @@
+package net.mikhailov.books.library.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    /**
+     * Первый вариант своей реализации UserDetailsService и PasswordEncoder через @Bean
+     *
+     * @return
+     */
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        var userDetailService = new InMemoryUserDetailsManager();
+//        var user = User.withUsername("evgenii")
+//                .password("test")
+//                .authorities("read")
+//                .build();
+//        userDetailService.createUser(user);
+//        return userDetailService;
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return NoOpPasswordEncoder.getInstance();
+//    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
+        http.authorizeRequests().anyRequest().authenticated();
+//        http.authorizeRequests().anyRequest().permitAll(); //разрешить все
+    }
+
+    /**
+     * Второй вариант  реализации UserDetailsService и PasswordEncoder через configure(auth)
+     *
+     * @param auth
+     * @throws Exception
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        var userDetailService = new InMemoryUserDetailsManager();
+        var user = User.withUsername("evgenii")
+                .password("test")
+                .authorities("read")
+                .build();
+        userDetailService.createUser(user);
+
+        auth.userDetailsService(userDetailService).passwordEncoder(NoOpPasswordEncoder.getInstance());
+
+        //Еще вариант :
+//        auth.inMemoryAuthentication()
+//                    .withUser("evgenii")
+//                    .password("test")
+//                    .authorities("read")
+//                .and()
+//                    .passwordEncoder(NoOpPasswordEncoder.getInstance());
+    }
+}
