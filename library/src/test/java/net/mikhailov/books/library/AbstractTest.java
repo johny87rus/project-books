a5c5b2d014e6ec3ba5beb1160312eb4a087a6606
emikhaylov <emikhaylov@diasoft.ru>
2022-01-16 22:18:41 +0500

    TEST 3

--- library/src/test/java/net/mikhailov/books/library/AbstractTest.java
+++ library/src/test/java/net/mikhailov/books/library/AbstractTest.java
@@ -29,8 +29,7 @@ public abstract class AbstractTest {
     protected RequestSpecification getRestAssuredAuthentificated() {
 
         /**
-         * TEST PIPELINE #2
-         */
+         * TEST PIPELINE #3         */
         var sessionId = given().auth().preemptive().basic("admin", password).contentType(ContentType.JSON).
                 when().get("/").
                 then().log().all().extract().response().getSessionId();
