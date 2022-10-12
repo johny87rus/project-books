c6d9469006f6cf7888c0d7baf340424fffcdf0e9
evgenii <mail@mikhailov.net>
2022-10-12 22:02:43 +0500

    Test commit4

--- library/src/main/java/net/mikhailov/books/library/LibraryServiceApplication.java
+++ library/src/main/java/net/mikhailov/books/library/LibraryServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LibraryServiceApplication {
-//TEST3
+//TEST4
     public static void main(String[] args) {
         SpringApplication.run(LibraryServiceApplication.class, args);
     }
