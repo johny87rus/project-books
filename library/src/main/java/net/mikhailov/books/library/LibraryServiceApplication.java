66bfcf75c53cf6d2b95a32b47124371abe86e2be
evgenii <mail@mikhailov.net>
2022-10-12 22:08:30 +0500

    clean

--- library/src/main/java/net/mikhailov/books/library/LibraryServiceApplication.java
+++ library/src/main/java/net/mikhailov/books/library/LibraryServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LibraryServiceApplication {
-//TEST6
     public static void main(String[] args) {
         SpringApplication.run(LibraryServiceApplication.class, args);
     }
